eea6263598be10121f2b592e9fe9b8b8d25018c2
Russell Macalintal <russell.macalintal@gmail.com>
2020-01-02 05:36:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
 
   def self.create_table
     sql = <<-SQL
-      CREATE TABLE students
+      CREATE TABLE IF NOT EXISTS students
       (id INTEGER PRIMARY KEY, name TEXT, grade TEXT)
     SQL
 
