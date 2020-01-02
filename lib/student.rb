1a056ec1b0fbf14b2d27cef922471622e5c72c55
Russell Macalintal <russell.macalintal@gmail.com>
2020-01-02 05:43:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,10 +42,10 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
-  def create(:name, :grade)
-    student = Student.new(:name, :grade)
-    student.save
-    student
-  end
+  # def create(:name, :grade)
+  #   student = Student.new(:name, :grade)
+  #   student.save
+  #   student
+  # end
 
 end
